cf227462bac94b8580f641994644928ac85cf067
Charles W Boswell <charliewboswell@gmail.com>
2025-08-09 06:49:13 -0400

    chore: remove unused imports

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
-from fastapi import UploadFile, File
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
